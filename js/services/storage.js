4a7c5ac57ef356b786489a8285581ae9bf04f139
Tamar Millgram <Tamarmillgram15@gmail.com>
2023-04-25 16:21:41 +0300

    First app setup

---
+++ js/services/storage.js
@@ -0,0 +1,14 @@
+'use strict'
+
+function saveMemesToStorage() {
+    saveToStorage('savedMemesDB', gSavedMemes);
+}
+
+function saveToStorage(key, val) {
+    localStorage.setItem(key, JSON.stringify(val))
+}
+
+function loadFromStorage(key) {
+    var val = localStorage.getItem(key)
+    return JSON.parse(val)
+}
